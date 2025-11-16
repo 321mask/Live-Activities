70fa8e38a38cfaf9fbb8d876fce6df67e2274d6a
Arseny <166810173+321mask@users.noreply.github.com>
2025-11-16 16:17:40 +0100

    Initial Commit

---
+++ Live Activities/Live_ActivitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Live_ActivitiesApp.swift
+//  Live Activities
+//
+//  Created by Arseny Prostakov on 16/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Live_ActivitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
